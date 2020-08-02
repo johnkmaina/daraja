ffbf8f3400b1cbbcb85da013b0da5a1e9e379490
Titus kamau <tituskamauit@gmail.com>
2020-08-02 21:30:52 +0300

    added register_url and simulate_c2b_transaction

---
+++ c2b.py
@@ -0,0 +1,46 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+from access_token import generate_access_token
+import keys
+ 
+def register_url():  
+ 
+  my_access_token = generate_access_token()
+  api_url = ("https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl")
+  
+  headers = {"Authorization": "Bearer %s" % my_access_token}
+  
+  request = { 
+      "ShortCode": keys.shortcode,
+      "ResponseType": " ",
+      "ConfirmationURL": "https://apiking.com/confirmation",
+      "ValidationURL": "https://apiking.com/validation_url"}
+  
+  response = requests.post(api_url, json = request, headers=headers)
+  
+  print(response.text)
+
+# register_url()
+
+
+def simulate_c2b_transaction():
+  
+    my_access_token = generate_access_token()
+    
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
+
+    headers = {"Authorization": "Bearer %s" % my_access_token}
+
+    request = { 
+      "ShortCode":keys.shortcode,
+      "CommandID":"CustomerPayBillOnline",
+      "Amount":"3",
+      "Msisdn":keys.test_msisdn,
+      "BillRefNumber":"123456" }
+    
+    response = requests.post(api_url, json = request, headers=headers)
+    
+    print(response.text)
+
+simulate_c2b_transaction()

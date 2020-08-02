356a7135a79ed702e85d0dde4bc035658cd8afb4
Titus kamau <tituskamauit@gmail.com>
2020-08-02 21:33:29 +0300

    added test_msisdn

---
+++ keys.py
@@ -0,0 +1,9 @@
+
+business_shortcode = "174379" #Lipa Na Mpesa Shortcode
+phone_number = "254725314691"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "puHvXftDRn0dxkRwQIACoIzsEGUdjsw7"
+consumer_secret = "zBPUb8bVNrrGpRl5"
+
+shortcode = "600277"
+test_msisdn = "254708374149"

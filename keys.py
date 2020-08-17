ba63631a40e741958ba3920c5c5596201e6a85e1
Titus kamau <tituskamauit@gmail.com>
2020-08-17 10:58:43 +0300

    'keys'

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 
 business_shortcode = "174379" #Lipa Na Mpesa Shortcode
-phone_number = "254725314691"
+phone_number = "254725314693"
 lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
 consumer_key = "puHvXftDRn0dxkRwQIACoIzsEGUdjsw7"
 consumer_secret = "zBPUb8bVNrrGpRl5"
